3a737d856362e69f3f78d007f2460e33f8dacec8
* <ninja.notleft@proton.me>
2025-11-12 01:03:59 +0330

    fix: updated crates
    Signed-off-by: * <ninja.notleft@proton.me>

--- src/qr.rs
+++ src/qr.rs
@@ -1,12 +1,19 @@
 use qrcode::QrCode;
-use image::{Luma, ImageBuffer};
+use image::Luma;
 use std::io::Cursor;
 
+/// Generates a QR code image as bytes (PNG format)
 pub fn generate_qr_png(data: &str) -> Vec<u8> {
-     let code = QrCode::new(data.as_bytes()).expect("Failed to create QR code");
+    let code = QrCode::new(data).expect("Failed to generate QR code");
+
+    // Render to an image buffer (Luma grayscale)
     let image = code.render::<Luma<u8>>().build();
 
+    // Encode to PNG in memory
     let mut buffer = Cursor::new(Vec::new());
-    image.write_to(&mut buffer, image::ImageFormat::Png).unwrap();
+    image
+        .write_to(&mut buffer, image::ImageFormat::Png)
+        .expect("Failed to write PNG");
     buffer.into_inner()
 }
+
