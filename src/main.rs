5b2e9b4db108672483235fdb4204337721993973
* <ninja.notleft@proton.me>
2025-11-11 14:51:20 +0330

    code: adde mqin bot runner
    Signed-off-by: * <ninja.notleft@proton.me>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+mod bot;
+mod qr;
+mod config;
+
+use teloxide::Bot;
+use config::get_bot_token;
+use bot::run_bot;
+
+#[tokio::main]
+async fn main() {
+    let token = get_bot_token();
+    let bot = Bot::new(token);
+
+    println!("Bot is running...");
+    run_bot(bot).await;
 }
+
